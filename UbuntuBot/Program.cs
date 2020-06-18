c25c8bb403ed962a73c3a2fef3bc2c7ad06db07e
Sergei Kamianetskii <i@ksergey.ru>
2020-06-18 07:10:10 +0300

    Добавьте файлы проекта.

---
+++ UbuntuBot/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Telegram.Bot;
+
+namespace UbuntuBot
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Start Application");
+
+            #region test
+
+            foreach (var url in SettingsApp.Urls)
+            {
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    Console.WriteLine($"{(int)response.StatusCode,5} {response.StatusCode,10} {url}");
+                    response.Close();                    
+                }
+                catch  
+                {
+                    Console.WriteLine($"no response - {url}");
+                }
+            }
+
+            var proxy = new WebProxy
+            {
+                Address = new Uri($"{SettingsApp.ProxyHost}:{SettingsApp.ProxyPort}"),
+                BypassProxyOnLocal = false,
+                UseDefaultCredentials = false,
+
+                //Credentials = new NetworkCredential(
+                //    userName: SettingsApp.ProxyUserName,
+                //    password: SettingsApp.ProxyPassword
+                //    )
+            };
+
+            HttpClient httpClient = new HttpClient(new HttpClientHandler { Proxy = proxy });
+
+            #endregion
+
+            TelegramBotClient bot = new TelegramBotClient(SettingsApp.Token, httpClient);
+
+            var botName = bot.GetMeAsync().Result.FirstName;
+
+            Console.WriteLine($"{botName} start...");
+
+            bot.OnMessage += (s, args) =>
+            {
+                string messageText = args.Message.Text ?? "null";
+                string userName = args.Message.Chat.FirstName;
+                long userId = args.Message.Chat.Id;
+
+                var response = String.Empty;
+
+                switch (messageText.ToLower())
+                {
+                    case "/os": response = $"OSVersion: {Environment.OSVersion}"; break;
+                    case "/me": response = $"{botName}"; break;
+                    default: response = "/os\n/me"; break;
+                }
+                bot.SendTextMessageAsync(
+                           chatId: userId,
+                           text: response ,
+                           replyToMessageId: args.Message.MessageId
+                           );
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ UbuntuBot/SettingsApp.cs
@@ -0,0 +1,34 @@
+﻿namespace UbuntuBot
+{
+    static class SettingsApp
+    {
+        static SettingsApp()
+        {
+            Urls = new string[]
+            {
+                "http://ya.ru/",
+                "http://vk.com/",
+                "http://google.com/",
+                "https://telegram.org/",
+                "https://api.telegram.org/"
+            };
+
+            Token = "{token}";
+          
+
+            ProxyUserName = "";
+            ProxyPassword = "";
+            ProxyHost = "http://80.187.140.26";
+            ProxyPort = "8080";
+
+        }
+
+        static public string[] Urls { get; private set; }
+        public static string Token { get; private set; }
+        static public string ProxyUserName { get; private set; }
+        static public string ProxyPassword { get; private set; }
+        static public string ProxyHost { get; private set; }
+        static public string ProxyPort { get; private set; }
+
+    }
+}
